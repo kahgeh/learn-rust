5b6037a65f928be839665b6aeb5f5712a02e3392
kahgeh.tan <kahgeh.tan@xero.com>
2021-06-18 11:26:11 +1000

    note down it's not enough to not access rc, it has to be dropped

--- my-redis/examples/importance-of-send-trait.rs
+++ my-redis/examples/importance-of-send-trait.rs
@@ -4,18 +4,28 @@ use tokio::task::yield_now;
 #[tokio::main]
 async fn main() {
     tokio::spawn(async {
-        let rc = Rc::new("hello");
-        println!("{}", rc);
-
+        let val = String::from("bizzaro");
+        {
+            let rc = Rc::new("hello");
+            println!("{}", rc);
+        }
         yield_now().await;
+        println!("{}", val);
 
         // Given that the async task, the continuation bit in this case can run in different thread
         // all state need to be implement the Send trait, so this will not compile because
         // Rc does not implement the Send trait
-        // note: most included types in rust implements the Send trait, so that it's values can be
+        // note 1: most included types in rust implements the Send trait, so that it's values can be
         // moved between threads - however this is not always the safe. Types that wrap around
         // underlying values so that it can be borrowed multiple times without ensuring atomicity in
         // the reference counting, e.g. Rc<T> are not safe and thus does not implement the Send trait
-        // println!("{}", rc);
+        /*
+        let rc = Rc::new("hello");
+        println!("{}", rc);
+        yield_now().await;
+        println!("continuation  - {}", rc);
+        */
+        // note 2: It is not enough to just not access rc, it has to be also dropped before the await
+
     });
 }
